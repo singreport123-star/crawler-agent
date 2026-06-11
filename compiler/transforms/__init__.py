1185408a11c80032cee4166a29e1c83bc129cf25
singreport123-star <singreport123@gmail.com>
2026-06-11 14:24:45 +0800

    Update __init__.py

--- compiler/transforms/__init__.py
+++ compiler/transforms/__init__.py
@@ -1,11 +1,10 @@
-from . import csrf_token, html_table
+from . import csrf_token, html_table, no_auth, extract_then_fetch
 
 TRANSFORMS = {
-    "csrf_token": csrf_token,
-    "html_table": html_table,
+    "csrf_token":        csrf_token,
+    "html_table":        html_table,
+    "no_auth":           no_auth,
+    "extract_then_fetch": extract_then_fetch,
 }
 
 __all__ = ["TRANSFORMS"]
-
-from . import no_auth
-TRANSFORMS["no_auth"] = no_auth
